91a3e4440a338e0d4be202b12e502d1cd4a9a4eb
Jackson Lewis <st.japa6@gmail.com>
2017-12-12 09:18:32 -0800

    Put common information into global file

---
+++ include/globals.h
@@ -0,0 +1,10 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+#include <string>
+#include <vector>
+
+const std::string LS_VERSION = "0.1";
+extern std::vector<std::string> CMD_OPTIONS;
+
+
+#endif
---
+++ src/globals.cpp
@@ -0,0 +1,14 @@
+#include "../include/globals.h"
+
+std::vector<std::string> CMD_OPTIONS  = { 
+    "help",
+    "version",
+    "build",
+    "create",
+    "install",
+    "load",
+    "search",
+    "remove",
+    "repo",
+    "status"
+};
