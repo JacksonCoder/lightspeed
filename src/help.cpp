d1c7232cd938262ef0f0cf49aea8ffbc591e94d9
Jackson Lewis <st.japa6@gmail.com>
2017-12-12 09:20:00 -0800

    Added files for commands

--- src/help.cpp
+++ src/help.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "../include/help.h"
 
-const std::string LS_VERSION = "0.1";
-
 void help () {
     std::cout << "LightSpeed v" + LS_VERSION << std::endl;
     std::cout << "Command help coming soon. In the meantime, you can read the README.md file for command information" << std::endl;
