8cc014ae15018c00caf32c2400668068d976c140
Jackson Lewis <st.japa6@gmail.com>
2017-12-23 17:01:10 -0800

    Linked LightSpeed modules to testing app

---
+++ lightspeed/tests/lsincludebundle.h
@@ -0,0 +1,15 @@
+#ifndef TEST_BUNDLE_H
+#define TEST_BUNDLE_H
+
+#include "../core/globals.h"
+#include "../core/include/enviroment.h"
+#include "../core/include/configparser.h"
+#include "../core/include/fileloader.h"
+#include "../core/include/file.h"
+#include "../core/include/cmakegenerator.h"
+#include "../core/include/codegrab.h"
+#include "../core/include/inputhandling.h"
+#include "../core/include/help.h"
+#include "../core/include/optionswitch.h"
+
+#endif
