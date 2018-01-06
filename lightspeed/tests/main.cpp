a440a4ed9c345033dc45cd2221a8c0ef5a4c4573
Jackson Lewis <st.japa6@gmail.com>
2018-01-06 06:01:58 -0800

    Extended tests

--- lightspeed/tests/main.cpp
+++ lightspeed/tests/main.cpp
@@ -1,11 +1,30 @@
 // Main testing file: contains all tests for LightSpeed
 #include "gtest/gtest.h"
 #include "lsincludebundle.h"
-TEST(InputHandling,FailsOnNoArguments) {
-    char* args[] = {"lightspeed",""};
+
+//
+// Utils testing
+//
+
+TEST(Utils,InputHandlerFailsOnNoArguments) {
+    char* args[1];
+    args[0] = "lightspeed";
     InputHandle i = InputHandle(1,args);
     ASSERT_TRUE(i.did_fail());
 }
+
+TEST(Utils,FailsOnNonexistentFile) {
+
+}
+
+TEST(Utils,FailsOnNoLightSpeedJSON) {
+    
+}
+
+
+//
+// Compilation testing
+//
 
 TEST(Compilation,CanBuildFileCorrectly) {
     EState* env = new EState();
@@ -36,4 +55,53 @@ TEST(Compilation,FailsOnNullInput) {
     cmakeg->compile();
     //ASSERT_TRUE(cmakeg->has_failed()); //Do this after inheritance is added.
 }
+
+//
+// Network testing
+//
+
+TEST(Network,IsConnectedToInternet) {
+
+}
+
+TEST(Network,CanCorrectlyFetchFile) {
+    
+}
+
+TEST(Network,FailsWithInvalidURL) {
+    
+}
+
+TEST(Network,FailsWithInvalidPath) {
+    
+}
+
+TEST(Network,NetworkBarPrintsCorrectly) {
+    /*
+    // Redirect STDOUT
+    std::stringstream buffer;
+    std::streambuf * old = std::cout.rdbuf(buffer.rdbuf());
+    displayBar(1,true);
+    std::cout.rdbuf(old);
+    ASSERT_TRUE(buffer.str()=="\n[>__________________________________________________________________________________________________]1%  ");
+    std::cout << std::flush;
+    */
+    std::stringstream buffer2;
+    std::streambuf * old2 = std::cout.rdbuf(buffer2.rdbuf());
+    displayBar(5,true);
+    std::cout.rdbuf(old2);
+    ASSERT_TRUE(buffer2.str()=="\n[---->______________________________________________________________________________________________]5%  ");
+    std::cout << std::flush;
+    std::stringstream buffer3;
+    std::streambuf * old3 = std::cout.rdbuf(buffer3.rdbuf());
+    displayBar(100,true);
+    std::cout.rdbuf(old3);
+    ASSERT_TRUE(buffer3.str()=="\n[--------------------------------------------------------------------------------------------------->]100%");
+    std::cout << std::flush;
+}
+
+
+//
+// Parsing testing
+//
 
